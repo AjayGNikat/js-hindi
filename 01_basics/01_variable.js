7022554df1157816ae1a0ec17fc6a3e23a35815f
AjayGNikat <ajitnikat7@gmail.com>
2025-08-08 13:58:34 +0000

    initial

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,2 +1,20 @@
 
-console.log("Welcome to Variables")
+const accountId=144553
+let accountEmail="Hitesh@gmail.com"   //reduces the scope problem
+var accountPassword="12345"
+
+accountCity="Jaipur"
+let accountState;
+//accountId=2;
+
+accountEmail="hc@hc.com"
+accountPassword="21212121"
+accountCity="Bengaluru"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/*
+prefer not to use var 
+beacuse of issue in block scope and functional scope.
+*/
