761dc861c26741fa9260fe9b8a14348f7cd6a4f0
AjayGNikat <ajitnikat7@gmail.com>
2025-08-10 05:18:32 +0000

    Initial Commit

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -2,8 +2,8 @@ let score="Hitesh";
 // console.log(typeof(score))
 
 let valueInNumber=Number(score)
-// console.log(typeof(valueInNumber))
-// console.log(valueInNumber);  //NaN
+console.log(typeof(valueInNumber))
+console.log(valueInNumber);  
 
 //"33"=>33
 //"33abc"=>Nan
