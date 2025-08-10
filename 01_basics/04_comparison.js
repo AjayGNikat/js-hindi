9b3469a68c3faf9454e5a0626c7e18487b965df9
AjayGNikat <ajitnikat7@gmail.com>
2025-08-10 05:54:47 +0000

    Initial Commit

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2" > 1);  //true js converts String into Number
+// console.log("02"> 1);  //true js converts String into Number
+
+console.log(null > 0);   //Comparison Coverts null to Number That Is 0
+console.log(null == 0);
+console.log(null >= 0);  //Comparison Coverts null to Number That Is 0 hence returns **True**
+
+console.log(undefined== 0); //false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
+
+// ===  "Strictly Check" not Only Value But dataypes also Will Check
+
+console.log("2" == 2);  //true
+console.log("2" === 2); //false
